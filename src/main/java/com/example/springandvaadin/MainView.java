2ae1608444479f50d3b62d3d1d8a3f44a970576e
Ondřej <sevcikon@gmail.com>
2019-04-02 10:30:44 +0200

    Customer editor and main view finished

--- src/main/java/com/example/springandvaadin/MainView.java
+++ src/main/java/com/example/springandvaadin/MainView.java
@@ -1,10 +1,13 @@
 package com.example.springandvaadin;
 
+import com.example.springandvaadin.editor.CustomerEditor;
 import com.example.springandvaadin.entity.Customer;
 import com.example.springandvaadin.repository.CustomerRepository;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.value.ValueChangeMode;
@@ -19,23 +22,54 @@ public class MainView extends VerticalLayout {
 
     private final CustomerRepository repo;
 
+    private final CustomerEditor editor;
+
     final Grid<Customer> grid;
 
     private TextField filterText = new TextField();
 
+    private final Button addNewBtn;
 
 
-    public MainView(CustomerRepository repo){
+    public MainView(CustomerRepository repo, CustomerEditor customerEditor){
 
 
         this.repo = repo;
+        this.editor = customerEditor;
         this.grid = new Grid<>(Customer.class);
-        add(filterText, grid);
+        this.addNewBtn = new Button("New customer", VaadinIcon.PLUS.create());
+
+
+        //build layout
+        HorizontalLayout actions = new HorizontalLayout(filterText,addNewBtn);
+
+        add(actions, grid, editor);
+
+        grid.setHeight("300px");
+        grid.setColumns("id","firstName", "lastName");
+        grid.getColumnByKey("id").setWidth("50px").setFlexGrow(0);
 
         filterText.setPlaceholder("Filter by last name");
+
+        // Hook logic to components
+
+        // Replace listing with filtered content when user changes filter
         filterText.setValueChangeMode(ValueChangeMode.EAGER);
         filterText.addValueChangeListener(e-> listCustomers(e.getValue()));
 
+        // Instantiate and edit new Customer the new button is clicked
+        grid.asSingleSelect().addValueChangeListener(e->editor.editCustomer(e.getValue()));
+
+        // Instantiate and edit new Customer the new button is clicked
+        addNewBtn.addClickListener(e->editor.editCustomer(new Customer("","")));
+
+        // Listen changes made by the editor, refresh data from backend
+        editor.setChangeHandler(()->{
+            editor.setVisible(false);
+            listCustomers(filterText.getValue());
+        });
+        // Initialize listing
+        listCustomers(null);
 
     }
 
